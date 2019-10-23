1ef7891ea0719c18705b2e2cb50b10914c35c0f6
Vladimir Vasic <vladimir@vasic.io>
2019-10-23 11:01:12 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Setting up Docker Compose"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /opt/bin/docker-compose
+sudo chmod +x /opt/bin/docker-compose
+
+echo "Please type the domain name without the https:// prefix:"
+read -p 'Domain name: ' domainvar
+echo "Please type the email address for renewal notices:"
+read -p 'Email address: ' emailvar
+echo "Setting up domain name in nginx.conf.."
+sed -i "s/[DOMAIN_NAME]/$domainvar/g" nginx.conf
+
+echo "Installing temporary SSL certificates with LetsEncrypt"
+
+docker run -it -p 80:80 -v $(pwd)/letsencrypt:/etc/letsencrypt certbot/certbot certonly --non-interactive --standalone --agree-tos -m $emailvar -d $domainvar
+
+echo "Setting up container auto-start service at boot time and daily update timer"
+
+cp *.service /etc/systemd/system/
+cp *.timer /etc/systemd/system/
+sudo systemctl enable /etc/systemd/system/start-container.service --now
+sudo systemctl start update.timer
